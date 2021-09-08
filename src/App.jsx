387c68246a5e9e31daa19fa0804134d8ceb01686
Nishant <nishant@loopinhq.com>
2021-09-08 18:54:23 +0530

    Rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import CakeContainer from "./components/CakeContainer";
+import { Provider } from "react-redux";
+import "./App.css";
+import store from "./redux/store";
+import HooksCakeContainer from "./components/HooksCakeContainer";
+import IceCreamContainer from "./components/IceCreamContainer";
+import WeedContainer from "./components/WeedContainer";
+import HooksWeedContainer from "./components/HooksWeedContainer";
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <CakeContainer />
+                <HooksCakeContainer />
+                <IceCreamContainer />
+                <WeedContainer />
+                <HooksWeedContainer />
+            </div>
+        </Provider>
+    );
+};
+
+export default App;
